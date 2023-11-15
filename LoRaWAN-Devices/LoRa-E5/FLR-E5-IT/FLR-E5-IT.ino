0fbc1d1dcade72b7225562e36e7f7d764ee96e0e
Ivan Tarozzi <itarozzi@gmail.com>
2023-11-15 19:35:28 +0100

    gestione del display solo definito in #define, #define per i vari pin a inizio sorgente, gestione iniziale del pulsante per decidere se collegarsi a TTN o ChirpStack - Ora usano AppKey diversi

--- LoRaWAN-Devices/LoRa-E5/FLR-E5-IT/FLR-E5-IT.ino
+++ LoRaWAN-Devices/LoRa-E5/FLR-E5-IT/FLR-E5-IT.ino
@@ -32,20 +32,54 @@
  */
 
 #include <Arduino.h>
-#include <Adafruit_SSD1306.h>
-
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#include <EasyButton.h>
 
 
 // Se togli questo commento, si attivano delle Serial.print
 // in giro per il programma che aiutano a capirci qualcosa in più
 // quando le cose non vanno...
 #define DEBUG_ON
+
+
+// Commentando questa define non utilizza il display (per chi ne e' sprovvisto)
+#define USE_TFT_DISPLAY
+
+
+#ifdef USE_TFT_DISPLAY
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define PIN_DISPLAY_SDA  8   // Il connettore I2C0 del Groove e' collegato ai pin 11 e 12 del pico, corrispondenti a GP8 e GP9
+#define PIN_DISPLAY_SCL  9
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#endif
+
+
+const long lorawan_interval = 10000;  // interval to send lorawan packets (milliseconds)
+
+#define PIN_UART1_TX  4  // Uart1 (Serial2) collegata al modulo LoRaWAN per i comandi AT
+#define PIN_UART1_RX  5
+#define UART1_BAUD    9600
+
+#define PIN_TEMP_SENSOR A0  // Grove - Temperature Sensor connected to A0
+#define PIN_HUMI_SENSOR A1  // Grove - Humidity Sensor connected to A1
+#define PIN_GREEN_LED D16   // Grove - green led connected to D16
+#define PIN_BLUE_LED D18   // Grove - green blue connected to D18
+
+#define BUTTON_PIN 20
+
+EasyButton button(BUTTON_PIN);
+
+// Button Callback.
+void onPressed() {
+    Serial.println("Button has been pressed!");
+}
+
+void onPressedForDuration() {
+    Serial.println("Button has been pressed for the given duration!");
+}
 
 static char recv_buf[512];
 static bool is_exist = false;
@@ -54,12 +88,11 @@ static int led = 0;
 
 const int B = 4275; // B value of the thermistor
 const int R0 = 100000; // R0 = 100k
-const int pinTempSensor = A0; // Grove - Temperature Sensor connected to A0
+// const int pinTempSensor = PIN_TEMP_SENSOR; // Grove - Temperature Sensor connected to A0
 
-const int pinHumiSensor = A1; // Grove - Humidity Sensor connected to A1
-
-const int pinGreenLed = D16; // Grove - green led connected to D16
-const int pinBlueLed = D18; // Grove -  blur led connected to D18
+// const int pinHumiSensor = A1; // Grove - Humidity Sensor connected to A1
+// const int pinGreenLed = D16; // Grove - green led connected to D16
+// const int pinBlueLed = D18; // Grove -  blur led connected to D18
 
 int led_status = LOW;
 
@@ -165,7 +198,7 @@ static void recv_parse(char* p_msg)
         else
             led_status = HIGH;
 
-        digitalWrite(pinGreenLed, led_status);
+        digitalWrite(PIN_GREEN_LED, led_status);
     }
 
     // qui decodifichiamo RSSI (potenza del segnale)
@@ -178,6 +211,9 @@ static void recv_parse(char* p_msg)
     if (p_start && (1 == sscanf(p_start, "SNR %d", &snr)))
         Serial.println("snr: " + String(snr));
 
+
+
+#ifdef USE_TFT_DISPLAY
 
     char signal_str[130];
     sprintf(signal_str, "RSSI:%d  SNR:%d", rssi, snr );
@@ -193,14 +229,32 @@ static void recv_parse(char* p_msg)
     display.setCursor(0, 9);     // Start at top-left corner
     display.println(signal_str);
     display.display();
+#endif
 
 }
 
-void initialize_LoRaModule()
+void initialize_LoRaModule(int lns_destination)
 {
-    Serial.println("Inizio config Modulo...");
+    Serial.print("Inizio config Modulo per NetworkServer:");
+    if (lns_destination == 0) {
+      Serial.println(" TTN");
+    }
+    else {
+      Serial.println(" ChirpStack");
+    }
 
-
+#ifdef USE_TFT_DISPLAY
+    display.clearDisplay();                                                      
+    display.setCursor(0,0);             // Start at top-left corner
+    display.print(F("LoRaWAN FLR-IT1"));
+    if (lns_destination == 0) {
+      display.println(F(" - TTN"));
+    }
+    else {
+      display.println(F(" - CS"));
+    }
+    display.display();
+#endif
 
     if (at_send_check_response((char*)"+AT: OK", 300, (char*)"AT\r\n")) {
         is_exist = true;
@@ -217,9 +271,18 @@ void initialize_LoRaModule()
 
         // Dati identificativi del sensore e dell'applicazione
         // -------------------------------------------------------------------- attento, lo spazio non serve ----------
-        at_send_check_response((char*)"+KEY: APPKEY 98FB", 1000, (char*)"AT+KEY=APPKEY,\"98fb76b567c9f6d20baa16e07990caac\"\r\n");
+        if (lns_destination == 0) {
+          // uso AppKey TTN
+          at_send_check_response((char*)"+KEY: APPKEY 98FB", 1000, (char*)"AT+KEY=APPKEY,\"98fb76b567c9f6d20baa16e07990caac\"\r\n");
+        }
+        else {
+          // uso AppKey ChripStack
+          at_send_check_response((char*)"+KEY: APPKEY 98FB", 1000, (char*)"AT+KEY=APPKEY,\"98fb76b567c9f6d20baa16e07990caaa\"\r\n");
+        }
         at_send_check_response((char*)"ID: DevEui, FC:A7", 1000, (char*)"AT+ID=DevEui,\"fca76b9eb3acf647\"\r\n");
-        at_send_check_response((char*)"ID: AppEui, 7B:9A", 1000, (char*)"AT+ID=AppEui,\"7b9a65aa20e645319b1298c03a31bc3b\"\r\n");
+
+        // Questo non sembra essere necessario (quantomeno con TTN -> indagare meglio)
+        // at_send_check_response((char*)"ID: AppEui, 7B:9A", 1000, (char*)"AT+ID=AppEui,\"7b9a65aa20e645319b1298c03a31bc3b\"\r\n");
 
         delay(200);
     } else {
@@ -240,29 +303,26 @@ void setup(void)
     Serial.begin(115200);
     Serial.print("FabLab Romagna E5 LORAWAN TEST - FLR Sensor IT1\r\n");
 
+
+    // pinMode(D25, OUTPUT);
+    pinMode(PIN_GREEN_LED, OUTPUT);
+    pinMode(PIN_BLUE_LED, OUTPUT);
+
+    // UART1 (Serial2) è collegata al nostro modulo sui PIN 4 e 5
+    Serial2.setTX(PIN_UART1_TX);
+    Serial2.setRX(PIN_UART1_RX);
+    Serial2.begin(UART1_BAUD);
+
+
+
+#ifdef USE_TFT_DISPLAY
     // Il connettore I2C0 del Groove e' collegato ai pin 11 e 12 del pico, corrispondenti a GP8 e GP9
-    Wire.setSDA(8);
-    Wire.setSCL(9);
+    Wire.setSDA(PIN_DISPLAY_SDA);
+    Wire.setSCL(PIN_DISPLAY_SCL);
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
       Serial.println(F("SSD1306 allocation failed"));
     }
-
-    // UART1 (Serial2) è collegata al nostro modulo sui PIN 4 e 5
-    Serial2.setTX(4);
-    Serial2.setRX(5);
-    Serial2.begin(9600);
-
-    initialize_LoRaModule();
-
-    pinMode(D25, OUTPUT);
-    pinMode(pinGreenLed, OUTPUT);
-    pinMode(pinBlueLed, OUTPUT);
-
-    digitalWrite(D25, HIGH);
-    digitalWrite(pinGreenLed, HIGH);
-    // digitalWrite(pinBlueLed, HIGH);
-
 
     display.clearDisplay();
     display.setTextSize(1);
@@ -271,60 +331,90 @@ void setup(void)
     display.setCursor(0,0);             // Start at top-left corner
     display.println(F("LoRaWAN FLR-IT1"));
     display.display();
+#endif
 
+    pinMode(BUTTON_PIN, INPUT);
+    int btnValue = digitalRead(BUTTON_PIN);
+
+    initialize_LoRaModule(btnValue);
+
+
+    // Initialize the button.
+    button.begin();
+
+    // Attach callback.
+    button.onPressed(onPressed);
+    // Attach callback for 2sec pression
+    button.onPressedFor(2000, onPressedForDuration);
+
+    // digitalWrite(D25, HIGH);
+    digitalWrite(PIN_GREEN_LED, HIGH);
 }
+
+
+unsigned long previousMillis = 0;  // will store last time lorawan message is sent
 
 void loop(void)
 {
 
-    int a = analogRead(pinTempSensor);
+    button.read();
+
+    int a = analogRead(PIN_TEMP_SENSOR);
     float R = 1023.0 / a - 1.0;
     R = R0 * R;
-
     float temp = 1.0 / (log(R / R0) / B + 1 / 298.15) - 273.15; // convert to temperature via datasheet
 
-    float humi = map(analogRead(pinHumiSensor), 0, 1023, 0, 100);
+    float humi = map(analogRead(PIN_HUMI_SENSOR), 0, 1023, 0, 100);
 
     uint8_t battery = map(random(10), 0, 10, 30, 100);
 
     int ok = 0;
 
     if (is_exist) {
-        if (!is_join) {
-            Serial.println("Modulo configurato.");
-            Serial.println("Tento un JOIN...");
-            ok = at_send_check_response((char*)"+JOIN: Network joined", 12000, (char*)"AT+JOIN\r\n");
-            if (ok) {
-                digitalWrite(pinGreenLed, LOW);
-                is_join = true;
-                Serial.println("JOIN riuscito.");
-            } else {
-                digitalWrite(pinGreenLed, HIGH);
-                is_join = false;
-                Serial.println("...JOIN non riuscito!\r\n");
-                delay(5000);
-            }
-        } else {
-            digitalWrite(pinBlueLed, HIGH);
-            Serial.println("Spedisco i dati...");
-            Serial.println("Humidity: " + String(humi) + ", Temperature: " + String(temp) + " *C, Battery: " + String(battery));
-            Serial.println("");
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= lorawan_interval) {
+          // save the last time you blinked the LED
+          previousMillis = currentMillis;
 
-            char cmd[128];
-            sprintf(cmd, "AT+CMSGHEX=\"%04X%04X%02X%02X\"\r\n", (int)(temp * 10), (int)humi, battery, led_status);
+          if (!is_join) {
+              Serial.println("Modulo configurato.");
+              Serial.println("Tento un JOIN...");
+              ok = at_send_check_response((char*)"+JOIN: Network joined", 12000, (char*)"AT+JOIN\r\n");
+              if (ok) {
+                  digitalWrite(PIN_GREEN_LED, LOW);
+                  is_join = true;
+                  Serial.println("JOIN riuscito.");
+              } else {
+                  digitalWrite(PIN_GREEN_LED, HIGH);
+                  is_join = false;
+                  Serial.println("...JOIN non riuscito!\r\n");
+                  delay(1000);
+              }
+          } 
+          
+          if (is_join) {
+              digitalWrite(PIN_BLUE_LED, HIGH);
+              Serial.println("Spedisco i dati...");
+              Serial.println("Humidity: " + String(humi) + ", Temperature: " + String(temp) + " *C, Battery: " + String(battery));
+              Serial.println("");
 
-            ok = at_send_check_response((char*)"+CMSGHEX: Done", 12000, cmd);
-            if (ok) {
-                Serial.println("\nRicevo dal server");
-                recv_parse(recv_buf);
-                Serial.println("");
-            } else
-                Serial.print("Spedizione del messaggio fallita...\r\n\r\n");
+              char cmd[128];
+              sprintf(cmd, "AT+CMSGHEX=\"%04X%04X%02X%02X\"\r\n", (int)(temp * 10), (int)humi, battery, led_status);
 
-            digitalWrite(pinBlueLed, LOW);
+              ok = at_send_check_response((char*)"+CMSGHEX: Done", 12000, cmd);
+              if (ok) {
+                  Serial.println("\nRicevo dal server");
+                  recv_parse(recv_buf);
+                  Serial.println("");
+              } else
+                  Serial.print("Spedizione del messaggio fallita...\r\n\r\n");
 
-            delay(10000);
+              digitalWrite(PIN_BLUE_LED, LOW);
+
+              delay(100);
+          }
         }
+
     } else {
         Serial.println("LoRa-E5 not found...");
         delay(1000);
